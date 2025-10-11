6b1850c02b8f6a325dcd92bd90b4cf7787f4b7de
mitchell-royan <mitchell.royan@my.jcu.edu.au>
2025-10-11 10:56:17 +1000

    Add commented answers to list questions

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,21 @@
+"""
+CP1404 - Practical 04
+"""
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# numbers[0] returns 3
+# numbers[-1] returns 2
+# numbers[3] returns 1
+# numbers[:-1] returns [3, 1, 4, 1, 5, 9] as it slices the last number
+# numbers[3:4] returns [1]
+# 5 in numbers returns True
+# 7 in numbers returns False
+# numbers + [6, 5, 3] returns [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+
+
+
+
+
+
