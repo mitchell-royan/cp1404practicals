994ef01c0318a1c760d66352bd070f134e568e4d
mitchell-royan <mitchell.royan@my.jcu.edu.au>
2025-10-11 11:01:14 +1000

    Add statements to change and print list

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -13,6 +13,18 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # 7 in numbers returns False
 # numbers + [6, 5, 3] returns [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
 
+# Change first element to the string ten
+numbers[0] = "ten"
+
+# Change last element to 1
+numbers[-1] = 1
+
+# Print all elements except first two
+print(numbers[2:])
+
+# Print whether 9 is an element of numbers
+print(9 in numbers)
+
 
 
 
