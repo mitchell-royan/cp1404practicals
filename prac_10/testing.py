5f11ee8968914d6504d72189db309cfd26b93ee5
mitchell-royan <mitchell.royan@my.jcu.edu.au>
2025-11-24 09:02:01 +1000

    Add answers to TODO comments

---
+++ prac_10/testing.py
@@ -0,0 +1,65 @@
+"""
+CP1404/CP5632 Practical
+Testing code using assert and doctest
+"""
+
+import doctest
+from prac_06.car import Car
+
+
+def repeat_string(s, n):
+    """Repeat string s, n times, with spaces in between."""
+    return " ".join([s] * n)
+
+
+def is_long_word(word, length=5):
+    """
+    Determine if the word is as long or longer than the length passed in
+    >>> is_long_word("not")
+    False
+    >>> is_long_word("supercalifrag")
+    True
+    >>> is_long_word("Python", 6)
+    True
+    """
+    return len(word) >= length
+
+
+def run_tests():
+    """Run the tests on the functions."""
+    # assert test with no message - used to see if the function works properly
+    assert repeat_string("Python", 1) == "Python"
+    # the test below should fail
+    assert repeat_string("hi", 2) == "hi hi"
+
+    car = Car()
+    assert car._odometer == 0, "Car does not set odometer correctly"
+
+    car = Car(fuel=10)
+    assert car.fuel == 10
+
+    car = Car()
+    assert car.fuel == 0
+
+
+def phrase_to_sentence(phrase):
+    """
+    Format a phrase as a sentence
+    >>> phrase_to_sentence('hello')
+    'Hello.'
+    >>> phrase_to_sentence('It is an ex parrot')
+    'It is an ex parrot.'
+    >>> phrase_to_sentence('I love this prac')
+    'I love this prac.'
+    """
+    sentence = phrase.capitalize()
+    if sentence[-1] != '.':
+        sentence = f"{sentence}."
+    return sentence
+
+
+run_tests()
+
+
+doctest.testmod()
+
